3d6f258fe5400bf47718a5b1e16d0280365e1189
sebatian Ramos <sebos.96@hotmail.com>
2017-11-29 16:20:57 +0100

    creacion ordenamiento

---
+++ ordenamiento2.0/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ordenamiento2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("METODO DE SELECCION");
+            int[] numeros = { 34, 56, 76, 87, 90, 1 };
+            int j;
+            int h;
+            int menor;
+            int aux;
+
+            // cogemos todo para organizar
+            for (j = 0; j < numeros.Length - 1; j++)
+            {
+                // tratando de organizando iniciando con el menor 
+                menor = j;
+                // el contador tendra valor extra a h
+                for (h = j + 1; h < numeros.Length; h++)
+                {
+                    if (numeros[h] < numeros[menor])
+                        menor = h;
+                }
+
+                // 
+                if (menor != j)
+                {
+                    aux = numeros[j];
+                    numeros[j] = numeros[menor];
+                    numeros[menor] = aux;
+                }
+
+                // Y muestro el progreso
+                foreach (int dato in numeros)
+                    Console.Write(dato + " ");
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
+        
+    
+
---
+++ por borbuja/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace por_borbuja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // incializamos variables en cero 
+            int j = 0;
+            int i = 0;
+            int[] numeros;
+            // pedimos la cantidad de numeros a ordenar
+            Console.WriteLine("cantidad deseada de ordenamiento");
+
+
+            j = int.Parse(Console.ReadLine());
+
+
+            numeros = new int[j];
+
+            //pedimos los numeros las veces que nos pide el usuario  
+            for (int a = 0; a < j; a++)
+            {
+                Console.Write("escriba el numero [{0}]:", a);
+                numeros[a] = int.Parse(Console.ReadLine());
+            }
+            // hacemos el ordenamiento comparando los 2 y enviando el menor atras
+            for (int b = 1; b < j; b++)
+            {
+                for (int c = 0; c < j - 1; c++)
+                {
+                    if (numeros[b] < numeros[c])
+                    {
+                        i = numeros[b];
+                        numeros[b] = numeros[c];
+                        numeros[c] = i;
+                    }
+                }
+            }
+            // mostramos el metodo uno por uno 
+            for (int r = 0; r < j; r++)
+                Console.WriteLine("{0}.-{1}", r + 1, numeros[r]);
+            Console.Read();
+        }
+    }
+}
